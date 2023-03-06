f9e05fe427b64315de9e335bc73ee47102eaf85e
dengjiawen <dengjiawen8955@gmail.com>
2023-03-06 13:19:41 +0800

    update: Auto commit And push

--- docs/config.js
+++ docs/config.js
@@ -1,3 +1,15 @@
 var repo = 'https://github.com/dengjiawen8955/docsify-template';
 var keywords = 'docsify,docsify-template,docsify-template,docsify-template';
 var description = 'docsify-template';
+
+var loadSidebar = true;
+
+
+// 百度统计
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?26aafdc51f546a6c9caca26e7174a692";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
