a84b91b1deb33ab5a42431e7a02875fd6d087b88
dengjiawen <dengjiawen8955@gmail.com>
2023-03-13 14:55:29 +0800

    feat: rm useless baidu ga

--- docs/config.js
+++ docs/config.js
@@ -5,11 +5,3 @@ var description = 'docsify-template';
 var loadSidebar = true;
 
 
-// 百度统计
-var _hmt = _hmt || [];
-(function () {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?26aafdc51f546a6c9caca26e7174a692";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
